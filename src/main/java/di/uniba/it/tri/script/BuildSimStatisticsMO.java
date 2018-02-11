2ca33b49f04f9cdced9ded885e76a0886db95b75
Pierpaolo <pierpaolo.basile@gmail.com>
2018-02-11 12:31:05 +0100

    - Fix statistics

--- src/main/java/di/uniba/it/tri/script/BuildSimStatisticsMO.java
+++ src/main/java/di/uniba/it/tri/script/BuildSimStatisticsMO.java
@@ -36,7 +36,6 @@ package di.uniba.it.tri.script;
 
 import di.uniba.it.tri.space.TemporalSpaceUtils;
 import di.uniba.it.tri.api.Tri;
-import di.uniba.it.tri.api.TriResultObject;
 import di.uniba.it.tri.vectors.Vector;
 import di.uniba.it.tri.vectors.VectorFactory;
 import di.uniba.it.tri.vectors.VectorReader;
