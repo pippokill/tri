25e5c2f804a6190181257e081db8da3036b74428
annalina <annalina.caputo@gmail.com>
2018-07-13 14:45:51 +0100

    .gitignore update to remove netbeans files and right version number updated to 0.7

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx3G -cp ./target/TemporalRandomIndexing-0.6-SNAPSHOT-jar-with-dependencies.jar $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15}
+java -Xmx3G -cp ./target/TemporalRandomIndexing-0.7-SNAPSHOT-jar-with-dependencies.jar $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15}
